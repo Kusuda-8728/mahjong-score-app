94f7d204d2110a90055d2777b9b5c329bf6ba8bd
Toshi <toshi@example.com>
2026-02-11 14:45:39 +0900

    Disable caching to ensure latest layout

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,19 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store, max-age=0, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
